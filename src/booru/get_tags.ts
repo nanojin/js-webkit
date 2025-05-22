9d4f27b61594ccb6c741be9e143d6c4c12e20736
Nano <nano@example.com>
2025-05-21 20:08:34 -0500

    moved, formatting

---
+++ src/booru/get_tags.ts
@@ -0,0 +1,26 @@
+Object.defineProperty(window, "TAGS", {
+	get: () => {
+		// rule34.xxx method
+		return Array.from(
+			// document.querySelectorAll("#tag-sidebar li:has(a[href*='tags'])"),
+			Array.from(document.querySelectorAll("#tag-sidebar li")).filter((li) =>
+				li.querySelector("a[href*='tags']")
+			),
+			(li) => {
+				const type = li.className
+					?.split(" ")
+					.find((cls) => cls.startsWith("tag-type-"))
+					?.replace("tag-type-", "");
+				const tag = li
+					.querySelector(`a[href*='tags']`)
+					?.innerHTML.replace(/[ ]/g, "_");
+				const rank = li.querySelector("span")?.innerHTML;
+				// console.log(tag, rank);
+				// return li
+				return { tag, rank, type };
+			}
+		);
+	},
+	configurable: false,
+	enumerable: true,
+});
